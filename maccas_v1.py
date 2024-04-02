1480852460caa64dd812ce0c6f748a4e07f0a3ea
pizzaboyyy <121211168+pizzaboyyy@users.noreply.github.com>
2024-04-02 14:23:44 +1300

    Made the combos
    Wrote out all the combos into dictonarys and made another dictonary that stored all the other dictonarys

---
+++ maccas_v1.py
@@ -0,0 +1,20 @@
+import easygui
+
+# Combos
+Value = {
+    "Beef Burger": 5.69,
+    "Fries": 1.00,
+    "Fizzy Drink": 1.00}
+Cheezy = {
+    "CheeseBurger": 6.69,
+    "Fries": 1.00,
+    "Fizzy Drink": 1.00}
+Super = {
+    "CheeseBurger": 6.69,
+    "Large Fries": 2.00,
+    "Smoothie": 2.00}
+
+Combos = [Value, Cheezy, Super]
+print(Combos)
+
+
