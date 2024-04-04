45209038bef17a94503974f84a275a197f8c1d88
pizzaboyyy <121211168+pizzaboyyy@users.noreply.github.com>
2024-04-04 14:34:11 +1300

    making the combos list look good
    ^^^

--- maccas_v2.py
+++ maccas_v2.py
@@ -2,20 +2,20 @@ import easygui
 
 # Combos
 Value = {
-    "Beef Burger": 5.69,
-    "Fries": 1.00,
-    "Fizzy Drink": 1.00}
+    "Beef Burger: 5.69\n",
+    "Fries 1.00\n",
+    "Fizzy Drink 1.00\n"}
 Cheezy = {
-    "CheeseBurger": 6.69,
-    "Fries": 1.00,
-    "Fizzy Drink": 1.00}
+    "CheeseBurger: 6.69\n",
+    "Fries 1.00\n",
+    "Fizzy Drink: 1.00\n"}
 Super = {
-    "CheeseBurger": 6.69,
-    "Large Fries": 2.00,
-    "Smoothie": 2.00}
+    "CheeseBurger: 6.69\n",
+    "Large Fries: 2.00\n",
+    "Smoothie: 2.00\n"}
 
 Combos = [Value, Cheezy, Super]
-print(Combos)
+print(easygui.msgbox(Combos))
 
 
 add_or_edit = easygui.buttonbox("Do You Want To Add Or Edit An Existing Menu?", choices=["Add", "Edit"])
