0adb97c36c54f72d0e17f6a3464c40c4fa493dd6
pizzaboyyy <121211168+pizzaboyyy@users.noreply.github.com>
2024-04-10 08:57:47 +1200

    trying to make a add function

--- maccas_v3.py
+++ maccas_v3.py
@@ -1,6 +1,23 @@
 import easygui
 
-# Combos
+#edit function
+
+def edit():
+    easygui.choicebox(Value)
+
+
+def add():
+    combo_name = easygui.enterbox("What do you want to call your combo")
+    items = easygui.enterbox("Please Enter the item then the price then a comma.")
+
+
+
+
+
+
+
+#Combos
+
 Value = {
     "Beef Burger: 5.69\n",
     "Fries 1.00\n",
@@ -17,10 +34,11 @@ Super = {
 Combos = [Value, Cheezy, Super]
 print(easygui.msgbox(Combos))
 
-
 add_or_edit = easygui.buttonbox("Do You Want To Add Or Edit An Existing Menu?", choices=["Add", "Edit"])
 
 if add_or_edit == "Edit":
-    print("e")
+    edit()
+elif add_or_edit == "Add":
+    add()
 
 print(Value)
